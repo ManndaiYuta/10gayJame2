d411a761b9fed875d5d76aca991d1a1fb6c8893d
k022g1008@g.neec.ac.jp <k022g1008@g.neec.ac.jp>
2023-09-05 15:25:52 +0900

    プロジェクト ファイルを追加します。

---
+++ Stage.h
@@ -0,0 +1,31 @@
+﻿#pragma once
+#include "Sprite.h"
+#include "ViewProjection.h"
+
+#include "Model.h"
+#include "WorldTransform.h"
+class Stage {
+public:
+	Stage();
+
+	~Stage();
+
+	void Initialize(ViewProjection view);
+
+	void Update();
+
+	void Draw2DFar();
+
+	//
+	void Draw3D();
+
+private:
+	uint32_t textureHandleBG_ = 0;
+	Sprite* spriteBG_ = nullptr;
+	// カメラ
+	ViewProjection viewProjection_;
+	// 地面
+	uint32_t textureHandoleStage_ = 0;
+	Model* modelStage_ = nullptr;
+	WorldTransform worldTransformStage_;
+};
